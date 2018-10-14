52e3581cc44e6eee3dbd680cb98a13abc12d185b
djordjeprzulj <przulj@gmail.com>
2018-10-14 19:36:26 +0200

    Zadatak 2.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'a6-v5-rx-pipes';
+
+  ngOnInit() {
+    const obs = interval(1500);
+  }
 }
