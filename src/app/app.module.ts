f508ef1630379baf60d9f7a706346fc97ab9b613
djordjeprzulj <przulj@gmail.com>
2018-10-14 20:46:18 +0200

    Zadatak 22.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PipesComponent } from './pipes/pipes.component';
+import { GreatPipe } from './great.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PipesComponent
+    PipesComponent,
+    GreatPipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/great.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GreatPipe } from './great.pipe';
+
+describe('GreatPipe', () => {
+  it('create an instance', () => {
+    const pipe = new GreatPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
